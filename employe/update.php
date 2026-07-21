d1e8b7d13b48c7953012002d86c4efa8af9f9406
Arellion <leo.pommarede@gmail.com>
2026-07-21 09:14:47 +0200

    Employe (#5)
    * feat:initialisation du projet
    
    * feat:ajout de la création d'employé, supression d'employé, et le début de l'update de l'employe
    
    * feat:ajout de la création d'employé, supression d'employé, et le début de l'update de l'employe

---
+++ employe/update.php
@@ -0,0 +1,56 @@
+<?php
+require_once "../header.php";
+$sql = "SELECT * FROM employe WHERE id_employe = ?";
+$stmt = $db->prepare($sql);
+$stmt->execute([$_GET['id']]);
+$employe = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php
+            if (!empty($employe)) :
+                ?>
+                <form action="<?= SITE_URL ?>/employe/result-update.php" method="POST">
+                    <div class="card">
+                        <div class="card-header">
+                            <h1>Modification de <?=$employe['nom_emp']?></h1>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingNom" placeholder="Nom" name="nom"
+                                       required value="<?=$employe['nom_emp']?>">
+                                <label for="floatingNom">Nom</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingPrenom" placeholder="Prenom"
+                                       name="prenom" required value="<?=$employe['prenom_emp']?>">
+                                <label for="floatingPrenom">Prenom</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingPoste" placeholder="Poste"
+                                       name="poste" required value="<?=$employe['poste_emp']?>">
+                                <label for="floatingPoste">Poste</label>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <input type="hidden" value="<?$employe['id_employe']?>">
+                            <button type="submit" class="btn btn-primary btn-sm">Ajouter</button>
+                        </div>
+                    </div>
+                </form>
+            <?php
+            else :
+                ?>
+                <div class="alert alert-danger">
+                    Aucun employé à cette identifiant.
+                </div>
+            <?php
+            endif;
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+require_once "../footer.php";
+?>
