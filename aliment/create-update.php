1d8e13e0d7f11bad72df41bcaeca4d2165327636
Arellion <leo.pommarede@gmail.com>
2026-07-22 16:56:34 +0200

    feat : Ajout d'une liste Aliment

--- aliment/create-update.php
+++ aliment/create-update.php
@@ -3,8 +3,8 @@ require_once "../header.php";
 print_r($_POST);
 ?>
 <?php
-$stmt = $db->query("SELECT * FROM aliment WHERE id_aliment = $_POST[id_aliment]")
-$stmt->fetch(PDO::FETCH_ASSOC)
+$stmt = $db->query("SELECT * FROM aliment WHERE id_aliment = $_POST[id_aliment]");
+$stmt->fetch(PDO::FETCH_ASSOC);
 ?>
     <div class="container">
         <div class="row">
@@ -15,7 +15,24 @@ $stmt->fetch(PDO::FETCH_ASSOC)
                             <?= isset($_POST['id_aliment']) ?'Modification d\'un nouvel aliment' : 'Création d\'un nouvel aliment' ?>
                         </div>
                         <div class="card-body">
-                            <input name="code_aliment" placeholder="Code aliment" value=""
+                            <div>
+                                <input type="text" class="form-control" name="code" placeholder="Code aliment"
+                                       value=""
+                                       id="floatingCode" required>
+                                <label for="floatingCode">Code Aliment</label>
+                            </div>
+                            <div>
+                                <input type="text" class="form-control" name="nom" placeholder="Nom aliment"
+                                       value=""
+                                       id="floatingNom" required>
+                                <label for="floatingNom">Nom aliment</label>
+                            </div>
+                            <div>
+                                <input type="text" class="form-control" name="stock" placeholder="Stock"
+                                       value=""
+                                       id="floatingStock" required>
+                                <label for="floatingStock">Stock</label>
+                            </div>
                         </div>
                         <div class="card-footer">
 
